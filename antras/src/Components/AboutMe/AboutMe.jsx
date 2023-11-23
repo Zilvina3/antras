e60deec4cbffe7171ad97087d5abd60af4f0a95b
ZilvProject <zzilvinas333@gmail.com>
2023-11-23 16:00:43 +0200

    edits

--- antras/src/Components/AboutMe/AboutMe.jsx
+++ antras/src/Components/AboutMe/AboutMe.jsx
@@ -1,6 +1,9 @@
 import React, {useEffect} from 'react'
 import { useNavigate } from 'react-router-dom'
 import { RevealOnScroll } from '../Layout/Footer/Footer';
+import './AboutMe.css'
+import neringaFt1 from './neringa1.jpg'
+import neringaFt2 from './neringa2.jpg'
 
 function AboutMe() {
 
@@ -12,7 +15,7 @@ function AboutMe() {
 
 
   return (
-    <main className=" bg-black font-serif min-h-screen">
+    <main className=" bg-black font-serif min-h-screen secondBg">
         <section className='container m-auto p-32 max-sm:p-8 animate-fade-up animate-duration-1500 animate-ease-in'>
 
               <h1 className='text-4xl text-center mb-20 text-orange-300'>Apie mane</h1>
@@ -22,8 +25,17 @@ function AboutMe() {
             <p className='p-5  text-2xl text-justify text-white max-sm:text-xl max-sm:text-start lg:my-14'>Prireikus trumpai papasakoti apie save, mes visi, tad ir aš, pirmiausia prisistatome vardu ir paminime tai, kas tariamai mus apibūdina. Tariamai, mat iš tiesų visi be išimties esame gerokai daugiau, nei patys manome. 
             </p>
 
+            <div className='flex justify-center'>
+              <img src={neringaFt1} alt="Neringa" width={500}/>
+            </div>
+           
+
             <p className='p-5  text-2xl text-justify text-white max-sm:text-xl max-sm:text-start lg:my-14'>Aš esu Neringa, Vilniaus universitete įgijusi chemikės – analitikės magistro laipsnį. Šias studijas pasirinkau, nes anuomet buvau įsitikinusi, kad tiksliuosiuose moksluose galiojanti griežta logika, padės man perprasti pasaulio tvarką ir priežasties bei pasekmės dėsnius. Visgi net ir gilinantis į chemijos mokslo paslaptis jaučiau didžiulį norą pažinti ir žmogiškojo proto slėpinius. Tad universitete lankiau visas su psichologija susijusias disciplinas. Sukauptos žinios paskatino sukti kiek netikėtu keliu – studijas tęsti Vytauto Didžiojo universitete pedagogikos studijų programoje. Visgi pedagoge savęs nevadinu. Man pačiai artimesnis gyvenimo mokytojos vaidmuo. Būtent todėl mentorių kursais stiprinau mokymo gebėjimą – padėti žmogui perprasti pačiam save ir išmokti priimti geriausius sprendimus.  
             </p>
+
+            <div className='flex justify-center'>
+              <img src={neringaFt2} alt="Neringa" width={500}/>
+            </div>
 
             <p className='p-5  text-2xl text-justify text-white max-sm:text-xl max-sm:text-start lg:my-14'>Diplomai man svarbūs ir brangūs, nes jiems atiduoda labai daug laiko ir pastangų, visgi, kad ir kaip banaliai beskambėtų, tikroji  mokykla egzistuoja už auditorijų durų. Tai yra nuolatinis tobulėjimas. Ir už jį įteikiamas ne dailus raštas, bet išmintis. Išmintis padeda atsiverti pasauliui ir patirti, kad visi turime beribių galimybių semti informaciją, vertingesnę už auksą. O tuos, kas vertinga, norisi dalintis. Juk dalijantis žiniomis, skirtingai nei auksu ar deimantais, šių kiekis nesumažėja, o padidėja. Dalijimąsi laikau savo pašaukimu ir svarbiausia šio gyvenimo užduotimi. Tad, jeigu skaitote šį mano prisistatymą, vadinasi turiu kažką, ko jums reikia.
             </p>
